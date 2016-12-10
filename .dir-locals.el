4011e3445ecbdf9a9ddbd1e0e86f2cb859976258
Kambiz Darabi <darabi@m-creations.net>
2016-12-10 13:05:09 +0100

    First version, copied from the current non-oss parent
    Note that many dependencies are present here which are not needed; a
    thorough cleanup will follow.

---
+++ .dir-locals.el
@@ -0,0 +1,8 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nxml-mode
+  (fill-column . 160)
+  (indent-tabs-mode . t)
+  (tab-width . 2)
+  (nxml-child-indent . 2)))
